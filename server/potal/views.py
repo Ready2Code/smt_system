84c8fcd09aa72d2e6f3c23425835fef07bd1c6d1
smt <sjtu@sjtu.com>
2017-05-08 11:30:49 +0800

    modify service settings

--- server/potal/views.py
+++ server/potal/views.py
@@ -3,9 +3,10 @@ from __future__ import unicode_literals
 
 from django.shortcuts import render
 from service_manager.models import ServiceSettings
+from service_manager.views import get_service_settings
 
 def start_page(request):
     #service_settings = ServiceSettings.objects.get(name="default")
-    service_settings = ServiceSettings.objects.all()[0]
+    service_settings = get_service_settings()
     return render(request, 'startpage.html', {'service_settings':service_settings})
 
--- server/service_manager/views.py
+++ server/service_manager/views.py
@@ -8,11 +8,20 @@ from signal_server import start_smt_system
 import threading
 from models import ServiceSettings
 
-service_settings,created = ServiceSettings.objects.get_or_create(name="default", programs='../related/programmes.json' , signal_destip='127.0.0.1',signal_port=10100, resource_broadcast_ip='127.0.0.1',resource_broadband_ip='127.0.0.1')
 #service_settings.save()
 
+def get_service_settings():
+    all_settings =  ServiceSettings.objects.all()
+    service_settings = 0
+    if len(all_settings) == 0:
+        service_settings,created = ServiceSettings.objects.get_or_create(name="default", programs='../related/programmes.json' , signal_destip='127.0.0.1',signal_port=10100, resource_broadcast_ip='127.0.0.1',resource_broadband_ip='127.0.0.1')
+    else:
+        service_settings = all_settings[0]
+    return service_settings 
+    
+
 def start_programs(request):
-    global service_settings    
+    service_settings = get_service_settings()   
 #    t= threading.Thread(target=start_smt_system,name='smt',args=('../related/programmes.json','192.168.1.41', 10100, '192.168.1.41', '192.168.1.22'))
     t= threading.Thread(target=start_smt_system,name='smt',args=(service_settings.programs, service_settings.signal_destip, service_settings.signal_port,service_settings.resource_broadcast_ip, service_settings.resource_broadband_ip))
     t.setDaemon(True)
@@ -25,7 +34,7 @@ def show_programs(request):
 
 
 def modify_service_settings(request):
-    global service_settings    
+    service_settings = get_service_settings()   
     service_settings.signal_destip   = request.GET['signal_destip']
     service_settings.signal_port     = request.GET['signal_port']
     service_settings.resource_broadcast_ip   = request.GET['resource_broadcast_ip']
