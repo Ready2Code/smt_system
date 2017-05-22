0a968127c3eefe020a4682355fa6a27dfd6472ce
jinxueji <xj.jin@nercdtv.org>
2017-05-22 17:48:55 +0800

    modify default settings of MonitorSettings

--- server/state_monitor/models.py
+++ server/state_monitor/models.py
@@ -16,9 +16,9 @@ def get_monitor_settings():
     if len(all_settings) == 0:
         settings,created = MonitorSettings.objects.get_or_create(monitor_name     ="default", 
                                                                  info_collector_ip='127.0.0.1',
-                                                                 info_collector_port=10100, 
+                                                                 info_collector_port=7777, 
                                                                  info_websocket_ip='127.0.0.1',
-                                                                 info_websocket_port=10100) 
+                                                                 info_websocket_port=7778) 
     else:
         settings = all_settings[0]
     return settings 
