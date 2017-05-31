48f65ee41157745846de1fbfcaaf998d5037624a
zhuzhao <zhuzhao@sjtu.edu.cn>
2017-05-31 11:18:15 +0800

    modify infosever settings

--- client/controller/models.py
+++ client/controller/models.py
@@ -16,9 +16,9 @@ def get_controller_settings():
     if len(all_settings) == 0:
         settings,created = ControllerSettings.objects.get_or_create(controller_name     ="default", 
                                                                  info_collector_ip='127.0.0.1',
-                                                                 info_collector_port=10100, 
+                                                                 info_collector_port=7777, 
                                                                  info_websocket_ip='127.0.0.1',
-                                                                 info_websocket_port=10100) 
+                                                                 info_websocket_port=7778) 
     else:
         settings = all_settings[0]
     return settings 
