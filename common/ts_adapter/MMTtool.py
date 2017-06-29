9e9faacb72693704e4c12f03b9f2207a0c6333a9
smthtpc <smthtpc@smthtpc.com>
2017-06-29 17:54:11 +0800

    use os.system to run MMTtool

--- common/ts_adapter/MMTtool.py
+++ common/ts_adapter/MMTtool.py
@@ -2,8 +2,29 @@
 from ctypes import *
 import thread
 from threading import Thread
+import os
+import re
 
 dll_list={}
+
+def kill_process_by_name(name):
+    print name
+    name = name.replace('-','')
+    name_list = name.split()
+    print name_list 
+    #cmd='ps aux | grep %s'%newname
+    cmd='ps aux | grep %s'%'| grep '.join(name_list) 
+    print cmd 
+    f=os.popen(cmd)
+    regex=re.compile(r'\w+\s+(\d+)\s+.*')
+    txt=f.read()
+    if len(txt)<5:
+        print 'there is no thread by name or command %s'%name
+        return
+    ids=regex.findall(txt)
+    cmd="kill %s"%' '.join(ids)
+    print cmd
+    os.system(cmd)
 
 def init():
     dll = CDLL("../related/libMMTtool.so")
@@ -21,29 +42,44 @@ def get_ip_and_port_from_addr(addr):
         return 0
     return addr_tuple 
 
+def ts_adapter_process(mode, srcaddr, destaddr):
+    ts_adapter_thread(mode, srcaddr, destaddr)
+    #t = Process(target=ts_adapter_thread,args=(mode, srcaddr, destaddr))
+    #t.setDaemon(True)
+    #t.start()
+ 
+
 def ts_adapter_thread(mode, srcaddr, destaddr):
-    global dll_list 
-    dll = init()
-    dll_list[srcaddr] = dll
+    global dll_list
     srcaddr_tuple=get_ip_and_port_from_addr(srcaddr)
     destaddr_tuple=get_ip_and_port_from_addr(destaddr)
-    dll.set_and_start.argtypes = [c_char_p, c_char_p, c_int, c_char_p, c_int]
-    dll.set_and_start( mode, srcaddr_tuple[0],srcaddr_tuple[1], destaddr_tuple[0],destaddr_tuple[1] )
-    return
+    command = "../related/MMTtool "
+    command = command + " --"+ mode
+    command = command + " --"+ "srcip " + srcaddr_tuple[0]
+    command = command + " --"+ "srcport " + str(srcaddr_tuple[1])
+    command = command + " --"+ "dstip " + destaddr_tuple[0]
+    command = command + " --"+ "dstport " + str(destaddr_tuple[1])
+    print command
+    dll_list[srcaddr] = command 
+    try:
+        os.system(command)
+        #p = Popen(shlex.split(ffmpeg_command), stdout=FNULL, stderr=FNULL)
+    except:
+        return
 
 def start_ts_adapter(mode, srcaddr, destaddr):
     global dll_list 
     if dll_list.has_key(srcaddr):
         return
-    t = Thread(target=ts_adapter_thread,args=(mode, srcaddr, destaddr))
+    t = Thread(target=ts_adapter_process,args=(mode, srcaddr, destaddr))
     t.setDaemon(True)
     t.start()
-    pass 
 
 def stop_ts_adapter(srcaddr):
     global dll_list 
     if dll_list.has_key(srcaddr):
-        dll_list[srcaddr].argtypes = [c_char_p]
-        dll_list[srcaddr].stop(srcaddr)
+        #dll_list[srcaddr].stop.argtypes = [c_char_p]
+        #dll_list[srcaddr].stop(srcaddr)
+        kill_process_by_name(dll_list[srcaddr])
         del dll_list[srcaddr]
-    
+
