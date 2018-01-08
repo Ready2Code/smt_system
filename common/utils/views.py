cf08e1bcfe6525bd7641a26a1f93011f7c3a5b1d
JinXueji <xj.jin@nercdtv.org>
2018-01-08 17:11:10 +0800

    modify debug print

--- common/utils/views.py
+++ common/utils/views.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+from datetime import datetime
 from django.shortcuts import render
 from django.http import HttpResponse
 import json
@@ -13,11 +14,20 @@ try:
 except:
     pass
 
+class DateTimeEncoder(json.JSONEncoder):
+    def default(self, o):
+        if isinstance(o, datetime):
+            return o.isoformat()
+        return json.JSONEncoder.default(self, o)
 
 # Create your views here.
 def debug(request):
-    print_str         = request.GET['print']
-    out_str = eval("'{}'.format("+print_str+")")
+    print_str         = request.GET['print'] if request.GET.has_key('print') else ''
+    value = eval(print_str)
+    try:
+        out_str =json.dumps({print_str:value}, indent=4, cls=DateTimeEncoder) 
+    except:
+        out_str = eval("'{}'.format("+print_str+")")
     return HttpResponse(out_str, content_type='application/json')
 
 
